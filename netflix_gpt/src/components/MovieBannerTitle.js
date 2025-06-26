4ee8713c49103a273c896864cbc88746e35b463f
vidhya112 <vidyacharpe5@gmail.com>
2025-06-26 23:02:05 +0530

    banner movie trailer

--- netflix_gpt/src/components/MovieBannerTitle.js
+++ netflix_gpt/src/components/MovieBannerTitle.js
@@ -1,13 +1,17 @@
-import React from "react";
+import { FaPlay, FaCircleInfo } from "react-icons/fa6";
 
 const MovieBannerTitle = ({ videoTitle, overview }) => {
   return (
-    <div className="pt-36 px-12 absolute w-screen aspect-video text-white">
-      <h1 className="text-black font-bold text-3xl">{videoTitle}</h1>
-      <p className="py-6 w-1/3">{overview}</p>
-      <div>
-        <button>Play</button>
-        <button className="px-8 py-3 text-md mx-2 text-white bg-gray-500 opacity-50 rounded-md font-bold">
+    <div className="pt-[20%] px-24 absolute w-screen aspect-video text-white bg-gradient-to-r from-black">
+      <h1 className="font-bold text-5xl">{videoTitle}</h1>
+      <p className="py-6 w-1/3 text-lg">{overview}</p>
+      <div className="flex">
+        <button className="bg-white text-black py-3 px-6 font-bold text-lg rounded-md flex hover:bg-opacity-70">
+          <FaPlay size={25} className="mr-2 mt-1" />
+          Play
+        </button>
+        <button className="px-7 py-3 text-lg mx-3 text-white bg-gray-500 rounded-md font-bold flex">
+          <FaCircleInfo size={25} className="mr-2 mt-1" />
           More Info
         </button>
       </div>
